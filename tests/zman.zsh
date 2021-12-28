6c01ec849617bcc2a8d53ca1192fc1aa030bd0c0
mattmc3 <mattmc3@gmail.com>
2021-12-28 09:49:31 -0500

    Add some tests

--- tests/zman.zsh
+++ tests/zman.zsh
@@ -1,7 +1,13 @@
 0=${(%):-%N}
 @echo "=== ${0:t:r} ==="
 
-source ${0:a:h:h}/zman.zsh
+@test "ZMAN_BROWSER is not set" -z "$ZMAN_BROWSER"
+@test "ZMAN_URL is not set" -z "$ZMAN_URL"
+@test "zman_lookup is not set" -z "$zman_lookup"
 
-result=$(zman)
-@test "zman succeeded" $? -eq 0
+source ${0:a:h:h}/zman.zsh
+@test "sourcing zman succeeded" $? -eq 0
+
+@test "ZMAN_BROWSER is set" -n "$ZMAN_BROWSER"
+@test "ZMAN_URL is set" "$ZMAN_URL" = "https://zsh.sourceforge.io/Doc/Release"
+@test "zman_lookup is an associative array" "${(t)zman_lookup}" = "association"
