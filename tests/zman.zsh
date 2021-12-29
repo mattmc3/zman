c245d51ba041db71ca61e8d77c9add3f3886ce9d
mattmc3 <mattmc3@gmail.com>
2021-12-28 22:54:44 -0500

    Add test for item counts

--- tests/zman.zsh
+++ tests/zman.zsh
@@ -1,5 +1,6 @@
 0=${(%):-%N}
 @echo "=== ${0:t:r} ==="
+PRJ_HOME=${0:A:h:h}
 
 @test "ZMAN_BROWSER is not set" -z "$ZMAN_BROWSER"
 @test "ZMAN_URL is not set" -z "$ZMAN_URL"
@@ -11,3 +12,7 @@ source ${0:a:h:h}/zman.zsh
 @test "ZMAN_BROWSER is set" -n "$ZMAN_BROWSER"
 @test "ZMAN_URL is set" "$ZMAN_URL" = "https://zsh.sourceforge.io/Doc/Release"
 @test "zman_lookup is an associative array" "${(t)zman_lookup}" = "association"
+
+lookup_count=$(wc -l $PRJ_HOME/lib/zman_lookup.zsh | awk '{print $1}')
+(( lookup_count = lookup_count - 2 ))
+@test "zman_lookup has correct number of items: '$lookup_count'" "${#zman_lookup[@]}" -eq $lookup_count
