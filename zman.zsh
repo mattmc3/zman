cd74edd61bb779036e044ac150ddc50819528c53
mattmc3 <mattmc3@gmail.com>
2021-12-29 09:25:04 -0500

    feat: zman autoload

--- zman.zsh
+++ zman.zsh
@@ -6,24 +6,9 @@
 0=${(%):-%N}
 
 () {
-  ZMAN_URL=${ZMAN_URL:-https://zsh.sourceforge.io/Doc/Release}
-  if [[ -n "$BROWSER" ]]; then
-    ZMAN_BROWSER=$BROWSER
-  elif [[ "$OSTYPE" == darwin* ]]; then
-    ZMAN_BROWSER=open
-  elif [[ "$OSTYPE" == cygwin* ]]; then
-    ZMAN_BROWSER=cygstart
-  else
-    ZMAN_BROWSER=xdg-open
-  fi
-
   local basedir="${${(%):-%x}:a:h}"
-  source $basedir/lib/zman_lookup.zsh
-  local funcdir="$basedir/functions"
-  fpath+="$funcdir"
-  local fnfile; for fnfile in "$funcdir"/*(.N); do
-    local fnname="${fnfile:t}"
-    (( $+functions[$fnname] )) && unfunction "$fnname"
-    autoload -Uz "$fnfile"
-  done
+  local fnfile="$basedir/functions/zman"
+  local fnname="${fnfile:t}"
+  (( $+functions[$fnname] )) && unfunction "$fnname"
+  autoload -Uz "$fnfile"
 }
